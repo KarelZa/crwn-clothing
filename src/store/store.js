61d4b7c5ef72cb2b4203d36832483d4ad323cd5d
KarelZa <karelzamazall@gmail.com>
2022-08-25 12:49:40 +0200

    Implementation of Redux devtools extension

--- src/store/store.js
+++ src/store/store.js
@@ -4,17 +4,23 @@ import logger from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+// Logger adds extra functionality to dispatch method -> catch action before hits the store and log out the state
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const composeEnhancer =
+	compose(
+		process.env.NODE_ENV !== 'production' &&
+			window &&
+			window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+	) || compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
 const persistConfig = {
 	key: 'root', // starting / persisting whole app
 	storage: storage,
 	blacklist: ['user'], // blacklisting user reducer,due to AuthStateListener
 };
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [logger]; // catch action before hits the store and log out the state
-const composedEnhancers = compose(applyMiddleware(...middleWares)); // compose allows us to pass multiple fce left to right, functional programing pattern
-
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
-
 export const persistor = persistStore(store); // persisting store
